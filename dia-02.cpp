7be85651d57c0c2226d6c641dfdee6f50d259900
Devfelipo <calielfelipo17@gmail.com>
2025-11-19 18:42:09 -0300

    Adiciona código do Dia 02 (primeiro programa em C++)

---
+++ dia-02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Dia 02 - Meu primeiro teste de C++ no GitHub!" << endl;
+
+    int a, b;
+    cout << "Digite dois numeros: ";
+    cin >> a >> b;
+
+    cout << "A soma de " << a << " + " << b << " = " << a + b << endl;
+
+    return 0;
+}
